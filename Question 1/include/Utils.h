4ae790e72edc7e6fbf69aab08170d7b068c707b4
ShreyasG16 <shreyas24087@iiitd.ac.in>
2024-09-29 15:42:13 +0530

    Removed Administrative Files(main), Academics Files and Escalation Matrix Files as thisbranch is Library Focussed

--- Question 1/include/Utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <string>
-
-// Function to trim whitespace from both ends of a string
-std::string trim(const std::string& str);
-
-#endif 
--- Question 2/include/Utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include "Member.h"
-
-void loadMembersFromCSV(const std::string& filename, Member* members, int& memberCount);
-
-#endif 
